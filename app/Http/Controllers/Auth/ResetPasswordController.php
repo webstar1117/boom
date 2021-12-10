650e40c57ef29594611e9096a996536042204bd8
webstar1117 <webstar1117@outlook.com>
2021-12-11 04:58:00 +0900

    change email verify

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -63,7 +63,7 @@ class ResetPasswordController extends Controller
     }
     function emailVerify($id){
         if($id){
-            $email=User::where($id)->first()->email;
+            $email=User::whereId($id)->first()->email;
             if($email){
                 return view('email-verify',[
                     'email'=>$email
